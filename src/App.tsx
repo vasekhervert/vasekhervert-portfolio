3b804adc90bce3e8c76bd65424c28540b01c30bf
vasekhervert <vasekhervert@gmail.com>
2022-12-06 23:18:28 +0100

    setting up homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import Button from "./components/Button";
 
+const navLinks: { label: string; path: string }[] = [
+  { label: "about", path: "/about" },
+  { label: "projects", path: "/projects" },
+  { label: "contact", path: "/contact" },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
+    <div className="bg-gray-900 h-screen w-screen text-white">
+      <header className="flex justify-between absolute top-0 left-0 w-screen p-8">
+        <a href="/" className="font-bold text-3xl relative">
+          VH<span className="text-xs text-sky-400 font-mono">.dev</span>
+        </a>
+        <nav className="font-mono text-sm">
+          {navLinks.map((link, index) => (
+            <a href={link.path} className="px-4">
+              <span className="text-sky-400">0{index + 1}.</span> {link.label}
+            </a>
+          ))}
+        </nav>
       </header>
+
+      <div className="grid grid-cols-12 max-w-7xl h-screen mx-auto items-center">
+        <div className="col-span-8 col-start-3">
+          <p className="font-mono text-sky-400">
+            Hi, my name is Václav Hervert.
+          </p>
+          <h1 className="text-6xl font-bold py-6 font-display">
+            I'm a frontend developer,
+          </h1>
+          <p className="max-w-xl leading-7 mb-8">
+            with an overflow to backend development and UX in heart. Pushing
+            buttons to create something great for more than a decade. Currently
+            looking for new challenges.
+          </p>
+          <Button label="Check out my projects" />
+        </div>
+      </div>
     </div>
   );
 }
