32abd3e4e8442565186d636b50c34fa358d2afff
vasekhervert <vasekhervert@gmail.com>
2022-12-07 09:52:07 +0100

    added hover styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,14 @@ import React from "react";
 type Props = {
   //children: JSX.Element;
   label: string;
+  classes: string;
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, classes }: Props) {
   return (
-    <button className="font-mono text-sm border border-sky-400 text-sky-400 rounded-md py-4 px-6 hover:bg-sky-400/25 transition-all hover:ease-in">
+    <button
+      className={`${classes} font-mono text-sm text-sky-400 py-2 hover:pl-1 transition-all underline hover:no-underline`}
+    >
       {label}
     </button>
   );
