699557987cbb7646cdd2e95d836d541fe4b5bdf5
vasekhervert <vasekhervert@gmail.com>
2022-12-09 10:47:36 +0100

    copy tweaks

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -7,18 +7,18 @@ export const experiences: {
   {
     range: "2017 - present",
     title: "Frontend dev - Freelance",
-    text: "Building apps that brings my clients bussiness closer to their customers. I work with technologies like React, Javascript, Firebase, TailwindCSS, Next.js, Node.js and others. Constantly learning more.",
+    text: "Building apps that help my clients to fulfill their bussiness goals. Work with technologies like React, JavaScript ES6+, Firebase, TailwindCSS, Next.js, Node.js and others. Constantly learning more.",
   },
   {
     range: "2016 - 2018",
     title: "Web designer/developer - Czech Promotion",
     link: "https://www.promotion.cz/",
-    text: "Worked in a small team, designed and developed a huge number of websites, most of them being custom WordPress templates. Also designed few e-commerce sites for clients like Scanquilt and Tebau. I also designed thousands of PPC banners.",
+    text: "Worked in a small team, designed and developed a huge number of websites, most of them being custom WordPress themes. Also designed few e-commerce sites for clients like Scanquilt and Tebau. I also designed thousands of PPC banners.",
   },
   {
     range: "2012 - 2016",
     title: "Web developer/designer - Freelance",
-    text: "Designed and developed sites for for individuals and small bussinesses using HTML and CSS, later I also got into WordPress. Also designed logos, bussiness cards, etc.",
+    text: "Designed and developed websites for for individuals and small bussinesses using HTML, CSS and JavaScript/jQuery, later I also started developing custom WordPress themes. Also designed logos, bussiness cards, etc.",
   },
 ];
 
